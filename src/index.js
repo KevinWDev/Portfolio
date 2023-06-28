fab5812095ebf79b8fbac53d363494d496620075
Kevin W <kevinwatteau@outlook.fr>
2023-06-28 10:23:53 +0200

    Modificationgh-pages, ajout basename

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import './utils/styles/index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/Portfolio'>
       <Header />
       <Routes> 
           <Route path='/' element={<Home />}></Route>
