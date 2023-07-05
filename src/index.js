627df4facfb79354b90e1aea8b01c25d4110a559
Kevin W <kevinwatteau@outlook.fr>
2023-07-05 11:54:28 +0200

    Ajout script index.html

--- src/index.js
+++ src/index.js
@@ -12,23 +12,32 @@ import Error from './pages/Error/index.jsx';
 import Infos from './pages/Infos/index.jsx';
 // Styles
 import './utils/styles/index.css';
+// 404
+const is404Page = window.location.pathname === '/404.html'
 
-
+// Root
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Router basename='/Portfolio'>
-      <Header />
-      <Routes> 
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/projets' element={<Projets />}></Route>
-          <Route path='/contact' element={<Contact />}></Route>
-          <Route path='/infos/:id/' element={<Infos />}></Route>
-          <Route path='*' element={<Error />}></Route>
-      </Routes>
-      <Footer />
-    </Router>
-    
-  </React.StrictMode>
-);
+
+if (is404Page) {
+  root.render(<Error />)
+} else {
+  root.render(
+    <React.StrictMode>
+      <Router basename='/Portfolio'>
+        <Header />
+        <Routes> 
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/projets' element={<Projets />}></Route>
+            <Route path='/contact' element={<Contact />}></Route>
+            <Route path='/infos/:id/' element={<Infos />}></Route>
+            <Route path='*' element={<Error />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
+      
+    </React.StrictMode>
+  );
+}
+
+
 
